1052f10471bc795c1377757bdac10b0857dc74db
Akmdrhannn <akh.raihanaf@gmail.com>
2023-04-06 23:26:45 +0700

    first commit

---
+++ app/Models/detail_transactions.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class detail_transactions extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'qty', 'sub_total'
+    ];
+}
---
+++ app/Models/users.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class users extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+        'avatar',
+        'first_name',
+        'last_name',
+        'username',
+        'email',
+        'password',
+        'jenis_kelamin',
+        'alamat',
+        'no_hp',
+        'role'
+    ];
+
+    protected $hidden = [
+        'password'
+
+    ];
+}
